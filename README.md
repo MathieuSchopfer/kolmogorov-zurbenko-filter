69c4ec2059621e19b6e4584a7c3e2d11b83fe8e2
Mathieu Schopfer <mathieu.schopfer@protonmail.ch>
2017-03-31 14:01:17 +0200

    Improving handling of series indices in KZFT

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Python implementation of the Kolmogorov Zurbenko filter
 As for now, this module is not provided with an installer. You can copy it into any of your projects.
 
 # Examples and documentation
-To get started, see the [API documentation](doc/html/index.html) and the [notebook example](notebooks/Kolmogorov-Zurbenko%20filter.ipynb).
+To get started, see the [API documentation](https://rawgit.com/MathieuSchopfer/kolmogorov-zurbenko-filter/master/doc/html/index.html) and the [notebook example](notebooks/Kolmogorov-Zurbenko%20filter.ipynb).
 
--- kolzur_filter.py
+++ kolzur_filter.py
@@ -244,10 +244,14 @@ def kzft(data, nu, m, k, t=None, dt=1.):
     :param list-like nu: Frequencies, length `Nnu`.
     :param int m: Filter window width.
     :param int k: Filter degree.
-    :param list-like t: Indices where to calculate the KZFT, of length `Nt`. If `None`, calculate over the whole
-      calculable range. 
+    :param list-like t: Calculation indices, of length `Nt`. If provided, KZFT filter will be calculated only for values
+      ``data[t]``. Note that the KZFT filter can only be calculated for indices in the range [k(m-1)/2, (N-1)-k(m-1)/2].
+      Trying to calculate the KZFT out of this range will raise an `IndexError`. `None`, calculation will happen over
+      the whole calculable range.
     :param float dt: Time step, if not 1.
-    :return: A :class:`numpy.ndarray` of shape `(Nnu, Nt)` or `(Nnu, N-k*(m-1))` if `t` is `None`.
+    :return: A :class:`numpy.ndarray` of shape `(Nnu, Nt)` or `(Nnu, N-k(m-1))` if `t` is `None`.
+    :raise IndexError: If `t` contains one or more indices out of the calculation range. See documentation of keyword
+      argument `t`.
 
     Given a time series :math:`X_t, t \\in \\{0, 1, \\cdots, N-1\\}`, the Kolmogorov-Zurbenko Fourier transform filter
     is defined for :math:`t \\in \\{\\frac{k(m-1)}{2}, \\cdots, N-1-\\frac{k(m-1)}{2}\\}` by
@@ -263,6 +267,14 @@ def kzft(data, nu, m, k, t=None, dt=1.):
         m = int((m-1)/dt+1)
         if not m%2:
             m += 1
+
+    if t is not None:
+        w = int(k*(m-1)/2)
+        t = np.asarray(t)-w
+        if np.any(t < 0) or np.any(t > (data.size-1-2*w)):
+            raise IndexError('Inpunt calculation indices are out of range. Calculation indices should be in the range '
+                             '[k*(m-1)/2, (N-1)-k*(m-1)/2], hence [{}, {}] in the present case.'
+                             .format(w, data.size-1-w))
 
     coef = _kz_coeffs(m, k)
     data = _kz_prod(data, coef, m, k, t=t)
@@ -309,15 +321,11 @@ def kzp(data, nu, m, k, dt=1.):
     w = k*(m-1)+1
 
     # Distance between two successve intervals
-    # l = (data.size-w)/(nt-1)
-    # l = int(l)
-
     l = int(m/10)
     nt = int((data.size-w)/l+1)
-    # print(l, w, data.size)
 
     # Calculation indices
-    l = np.arange(nt-1)*l
+    l = np.arange(nt-1)*l+k*(m-1)/2
     l = np.floor(l).astype(int)
 
     return np.sqrt(np.nanmean(np.square(2*np.abs(kzft(data, nu, m, k, t=l))), axis=-1))
